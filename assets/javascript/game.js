5c399202dbc8d70538758275af1e60afa7b59f61
Matt Michael <matthewemichael@gmail.com>
2019-08-15 14:10:13 -0500

    updated css/html, added images, pseudo coded js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,9 @@
-$( document ).ready(function() {
-    
-
-
-
-
-
-
-
-
-
-
-    
-});
+// Generate random number to guess
+// Generate random number for each crystal
+// Variables to keep track of wins, losses and player score
+// Display random number
+// Display wins
+// Display losses
+// Display player score
+// Click on crystals
+// Reset Game
